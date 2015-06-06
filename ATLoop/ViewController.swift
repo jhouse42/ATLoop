fcbc5a168105b571e9fc7890f0efd93bd5da8106
Jeanie House <j.house1028@gmail.com>
2015-06-06 13:14:08 -0400

    Update
    Added webView and TabBarController

--- ATLoop/ViewController.swift
+++ ATLoop/ViewController.swift
@@ -6,13 +6,23 @@
 //  Copyright (c) 2015 Jeanie House. All rights reserved.
 //
 
+
+// http://store.samhsa.gov/shin/content/SMA13-4742/Overdose_Toolkit_2014_Jan.pdf
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL (string: "http://store.samhsa.gov/shin/content/SMA13-4742/Overdose_Toolkit_2014_Jan.pdf");
+        let requestObj = NSURLRequest(URL: url!);
+        webView.loadRequest(requestObj);
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
